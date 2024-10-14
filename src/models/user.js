9741b3ad19c8d7a7a4c857deced384dfc3558669
Fernando Domeneghetti <fernandomirandadomeneghetti@hotmail.com>
2024-10-14 16:57:53 -0300

    refator: replace data type to auto generate id

--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,10 @@ const sequelize = require('../config/database');
 
 const User = sequelize.define('Users', {
     id: {
-        type: DataTypes.UUIDV4,
-        primaryKey: true
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false
     },
     username: {
         type: DataTypes.STRING,
