61ddd4ae6e78b03c7275929f9b31fe91ff58e4fa
Fernando Domeneghetti <fernandomirandadomeneghetti@hotmail.com>
2024-10-21 18:22:53 -0300

    doc: add swagger

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,40 +1,34 @@
 const express = require('express');
-const userService = require('../services/userService');
-const authenticateToken = require('../middleware/auth');
-
 const router = express.Router();
-router.post('/register', async(req, res) =>{
-    try{    
-        console.log('------- body', req.body);    
+const userService = require('../services/userService');
+
+router.post('/register', async (req, res) => {
+    try {
         const { username, password } = req.body;
         const user = await userService.register(username, password);
         res.json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-})
+});
 
-router.post('/login', async(req, res) => {
-    try{
+router.post('/login', async (req, res) => {
+    try {
         const { username, password } = req.body;
         const token = await userService.login(username, password);
-        res.json(token);
+        res.json({ token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-})
+});
 
-router.get('/users', authenticateToken, async (req,res)=>{
-    try{
+router.get('/users', async (req, res) => {
+    try {
         const users = await userService.getUsers();
         res.json(users);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-
 });
 
 module.exports = router;
